76e51d9047c4a1bde25b7ea10500f039000e870d
zAndrezz <84895875+zAndrezz@users.noreply.github.com>
2021-11-29 12:06:33 -0600

    task 5.2

--- App.js
+++ App.js
@@ -1,27 +1,22 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-//importing the screens
-import Start from "./components/Start";
+import React, { Component } from "react";
+// import { StyleSheet, View, Text, TextInput, Button } from "react-native";
 import Chat from "./components/Chat";
-//importing react native gesture handler
+import Start from "./components/Start";
+// import react native gesture handler
 import "react-native-gesture-handler";
-//importing react Navigation
+
+// import react Navigation
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
-//Component for switching between pages
+const Stack = createStackNavigator();
+
 export default class App extends React.Component {
   render() {
-    const Stack = createStackNavigator();
     return (
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Start">
-          <Stack.Screen
-            name="Start"
-            component={Start}
-            options={{ title: "Welcome to Texter!" }}
-          />
+          <Stack.Screen name="Start" component={Start} />
           <Stack.Screen name="Chat" component={Chat} />
         </Stack.Navigator>
       </NavigationContainer>
--- components/Chat.js
+++ components/Chat.js
@@ -1,29 +1,113 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  View,
+  Button,
+  StyleSheet,
+  Platform,
+  KeyboardAvoidingView,
+} from "react-native";
+import { Bubble, GiftedChat } from "react-native-gifted-chat";
 
-class Chat extends Component {
+export default class Chat extends Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    };
+  }
+
   componentDidMount() {
-    let { name } = this.props.route.params;
-    this.props.navigation.setOptions({ title: name });
+    this.setState({
+      //mock-data
+      messages: [
+        {
+          _id: 4,
+          text: "Hello!",
+          createdAt: new Date(),
+          user: {
+            _id: 1,
+            name: this.props.route.params.name,
+          },
+        },
+        {
+          _id: 3,
+          text: this.props.route.params.name + " has joined the chat!",
+          createdAt: new Date(),
+          system: true,
+        },
+        {
+          _id: 2,
+          text: "Hello developer",
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: "https://placeimg.com/140/140/any",
+          },
+        },
+        {
+          _id: 1,
+          text: "ChatBot has joined the chat!",
+          createdAt: new Date(),
+          system: true,
+        },
+      ],
+    });
+  }
+
+  //changing the text bubble color
+  renderBubble(props) {
+    let bgColor = this.props.route.params.bgColor; //choosed color in start view
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          //user's color
+          right: {
+            backgroundColor: bgColor,
+          },
+          //chat partner's color
+          left: {
+            backgroundColor: "#555",
+          },
+        }}
+      />
+    );
+  }
+
+  //add new messages to 'message' array
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
   }
 
   render() {
-    let { bgColor } = this.props.route.params;
+    //inputted name from Start screen
+    let name = this.props.route.params.name;
+    //display the name in header as screen title in Chat
+    this.props.navigation.setOptions({ title: name });
     return (
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: bgColor,
-        }}
-      >
-        <Text>Chat screen</Text>
+      <View style={styles.chatContainer}>
+        <GiftedChat
+          renderBubble={this.renderBubble.bind(this)}
+          messages={this.state.messages}
+          onSend={(messages) => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
+        {/* fix for older Android devices where the input field is hidden beneath the keyboard. */}
+        {Platform.OS === "android" ? (
+          <KeyboardAvoidingView behavior="height" />
+        ) : null}
       </View>
     );
   }
 }
-
-export default Chat;
-
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  chatContainer: {
+    flex: 1,
+    backgroundColor: "#222",
+  },
+});
